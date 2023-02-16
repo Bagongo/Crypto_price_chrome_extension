0d2fb02b74f8552500c0ea6333770f0bb8687004
Ale <akak@hotmail.it>
2023-02-16 15:07:37 +0100

    Update file structure

--- background.js
+++ background.js
@@ -8,21 +8,14 @@ chrome.runtime.onInstalled.addListener(function(details){
     }
 });
 
-// let callCounter = 0;
-
-// const updateCallCount = () => {
-//     callCounter++;
-//     console.log(callCounter);
-// };
-
-// setTimeout(updateCallCount, 100);
-
+//formats the price to be shown in proper shortened version on the ext badge  
 const formatPrice = n => {
     if (n >= 1e6) return String(+(n / 1e6).toFixed(1)) + "M";
     if (n >= 1e3) return String(+(n / 1e3).toFixed(1)) + "K";
     if (n < 1e3) return String(n);
 };
 
+//updates the bitcoin price to be shown in the badge 
 const refreshBadge = () => {
     let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&precision=2"
     fetch(url).then(response => response.json()).then(response => {
--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-//const API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&precision=2";
-
 const generateURL = (coin) => {
     return `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd&precision=2`
 };
