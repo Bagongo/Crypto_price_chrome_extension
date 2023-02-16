0331cd3d12dfa22ce578db520289813954c10be0
Ale <akak@hotmail.it>
2023-02-16 14:05:28 +0100

    added badge update function

--- script.js
+++ script.js
@@ -11,19 +11,28 @@ const formatPrice = n => {
 };
 
 const callAPI = (url,coin) => { fetch(url).then(response => response.json()).then(response => {
-    console.log(response[coin]["usd"]);
-    let cell = document.getElementById(coin);
-    let price = String(response[coin]["usd"]);
-    cell.innerHTML = price;
-});
-}
+        let cell = document.getElementById(coin);
+        let price = String(response[coin]["usd"]);
+        cell.innerHTML = price;
+    });
+};
+
+const refreshBadge = () => {
+    fetch(generateURL("bitcoin")).then(response => response.json()).then(response => {
+        let price = formatPrice(response["bitcoin"]["usd"]);
+        chrome.action.setBadgeText({text: price});
+    });
+};
 
 document.querySelectorAll('.coin').forEach(function(span) {
-    console.log(span);
     let coin = String(span.id);
     let url = generateURL(coin)
     callAPI(url, coin); 
 });
+
+refreshBadge();
+
+
 
 
 
