102197c3964ebc106245f257859dc14e24d48a55
Ale <akak@hotmail.it>
2023-02-16 13:36:55 +0100

    added multi coin basic functionality.

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const btc = document.getElementById("bitcoin");
+//const API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&precision=2";
 
-console.log("working!");
-
-const API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&precision=2";
+const generateURL = (coin) => {
+    return `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd&precision=2`
+};
 
 const formatPrice = n => {
     if (n >= 1e6) return String(+(n / 1e6).toFixed(1)) + "M";
     if (n >= 1e3) return String(+(n / 1e3).toFixed(1)) + "K";
     if (n < 1e3) return String(n);
 };
-  
-const callAPI = () => { fetch(API_URL).then(response => response.json()).then(response => {
-    console.log(response)
-    let price = String(response.bitcoin.usd);
-    btc.innerHTML = price;
-    chrome.action.setBadgeText({text: formatPrice(price)});
-    });
+
+const callAPI = (url,coin) => { fetch(url).then(response => response.json()).then(response => {
+    console.log(response[coin]["usd"]);
+    let cell = document.getElementById(coin);
+    let price = String(response[coin]["usd"]);
+    cell.innerHTML = price;
+});
 }
 
+document.querySelectorAll('.coin').forEach(function(span) {
+    console.log(span);
+    let coin = String(span.id);
+    let url = generateURL(coin)
+    callAPI(url, coin); 
+});
 
-callAPI();
-setTimeout(callAPI, 10000);
 
 
