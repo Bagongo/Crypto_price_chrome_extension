de468d2482f0a904093e72d0d52226b41c38f726
Ale <akak@hotmail.it>
2023-11-02 13:56:43 +0100

    readded badge init and  refresh logic (working). Set up refreshing (badge + coin data)

--- background.js
+++ background.js
@@ -2,7 +2,20 @@
 let numOfTopCoins = 3;
 
 // Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("This is a first install!");
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+    }
+    initBadge();
+});
 
+const initBadge = () => {
+    chrome.action.setBadgeBackgroundColor({ color:'#1B73E8'});
+    refreshBadge();
+};
 
 //fetch data for the top coins
 const getTopCoins = (num) => {
@@ -19,20 +32,20 @@ const formatPrice = n => {
     if (n < 1e3) return String(n);
 };
 
-
-
 //updates the bitcoin price to be shown in the badge 
 const refreshBadge = () => {
     let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&precision=2"
     fetch(url).then(response => response.json()).then(response => {
         let price = formatPrice(response["bitcoin"]["usd"]);
         let currTime = new Date();
-        console.log("badge reset at: " + currTime.toLocaleString());
+        console.log("badge refreshed at: " + currTime.toLocaleString());
         chrome.action.setBadgeText({text: price});
     });
 };
 
+// getTopCoins(numOfTopCoins);
+
 //handles refreshing
 const dataRefreshRate = 60;
-getTopCoins(numOfTopCoins);
-//setInterval(() => getTopCoins(numOfTopCoins), 1000 * dataRefreshRate);
+setInterval(() => refreshBadge(), 1000 * dataRefreshRate);
+setInterval(() => getTopCoins(numOfTopCoins), 1000 * dataRefreshRate);
